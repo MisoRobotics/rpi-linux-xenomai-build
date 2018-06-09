8cd5dbdc4a918fc10f1b8a61cfbd1f11ace714d4
Ryan Sinnet <rsinnet@misorobotics.com>
2018-06-09 13:45:36 -0700

    Cleaning up build-4.9.sh.

--- build-4.9.sh
+++ build-4.9.sh
@@ -6,17 +6,16 @@
 readonly RED='\033[0;31m'
 readonly NOCOLOR='\033[0m'
 
-readonly THISDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-readonly BASEDIR=/tmp/rpi
+readonly NUM_CORES=$(cat /proc/cpuinfo | grep processor | wc -l)
+
+readonly THISDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+readonly BASEDIR="/tmp/rpi"
 readonly TOOLSDIR="${BASEDIR}/tools"
 readonly LINUXDIR="${BASEDIR}/linux"
 readonly XENODIR="${BASEDIR}/xenomai"
 readonly IPIPEPATCH="${THISDIR}/patches/ipipe-core-4.9.80-arm-5.patch"
 readonly MODDIR="${BASEDIR}/modules"
 
-readonly FIRMWARE="5db8e4e1c63178e200d6fbea23ed4a9bf4656658"
-
-readonly BOOTDIR="/media/rsinnet/boot"
 
 error_exit()
 {
@@ -27,7 +26,7 @@ error_exit()
 
 install_deps()
 {
-  sudo apt-get install -y gcc-4.7-arm-linux-gnueabi libncurses5-dev
+  sudo apt-get install -y gcc-4.7-arm-linux-gnueabi libncurses5-dev bc
 }
 
 get_sources()
@@ -40,21 +39,17 @@ get_sources()
     error_exit "Failed to make directory ${BASEDIR}"
   fi
 
-  cd ${BASEDIR}
-
-  echo "Cloning Raspberry Pi tools."
+  echo "Cloning Raspberry Pi tools"
   if ! git clone https://github.com/raspberrypi/tools.git ${TOOLSDIR}; then
     error_exit "Failed to clone Raspberry Pi tools"
   fi
 
-  echo "Cloning linux kernel source."
-  if ! git clone https://github.com/raspberrypi/linux.git ${LINUXDIR}; then
+  echo "Cloning linux kernel source"
+  if ! git clone --depth 1 --branch rpi-4.9.y https://github.com/raspberrypi/linux.git ${LINUXDIR}; then
     error_exit "Failed to clone Raspberry Pi Linux into ${LINUXDIR}"
   fi
 
-  cd ${LINUXDIR}
-
-  echo "Cloning Xenomai source."
+  echo "Cloning Xenomai source"
   if ! git clone --depth 1 --branch stable-3.0.x https://git.xenomai.org/xenomai-3.git ${XENODIR}; then
     error_exit "Failed to clone Xenomai stable-3.0.x source"
   fi
@@ -62,7 +57,7 @@ get_sources()
 
 prepare_kernel()
 {
-  echo "Patching linux kernel."
+  echo "Patching linux kernel"
   if ! cd ${XENODIR}; then
     error_exit "Failed to cd to xenomai source tree at ${XENODIR}"
   fi
@@ -82,21 +77,30 @@ build_kernel()
     error_exit "Failed to make directory ${MODDIR}"
   fi
 
-  if ! make ARCH=arm CROSS_COMPILE=/usr/bin/arm-linux-gnueabi- bcm2709_defconfig -j20; then
-    error_exit "Failed to make default configuration for Raspberry Pi 3."
+  local ARM_ARGS=" ARCH=arm CROSS_COMPILE=/usr/bin/arm-linux-gnueabi- -j${NUM_CORES} "
+
+  echo "Copying default configuration for bcm2709 (Raspberry Pi 2/3)"
+  if ! make ${ARM_ARGS} bcm2709_defconfig; then
+    error_exit "Failed to make default configuration for Raspberry Pi 3"
   fi
 
-  if ! make ARCH=arm CROSS_COMPILE=/usr/bin/arm-linux-gnueabi- -j20 zImage modules dtbs; then
+  echo "Building Linux"
+  if ! make ${ARM_ARGS} zImage modules dtbs; then
     error_exit "Failed to build linux kernel"
   fi
 
-  if ! make modules_install ARCH=arm CROSS_COMPILE=/usr/bin/arm-linux-gnueabi- INSTALL_MOD_PATH=${MODDIR} -j20; then
+  echo "Installing modules to ${MODDIR}"
+  if ! make modules_install ${ARM_ARGS} INSTALL_MOD_PATH=${MODDIR}; then
     error_exit "Failed to make modules_install"
   fi
+}
 
+deploy_kernel()
+{
   if ! cp arch/arm/boot/dts/*.dtb ${BOOTDIR}/; then
     error_exit "Failed to copy dtbs to boot directory"
   fi
+
   if ! cp arch/arm/boot/dts/overlays/*.dtb* ${BOOTDIR}/overlays/; then
     error_exit "Failed to copy overlay dtbs to boot directory"
   fi
@@ -110,25 +114,11 @@ build_kernel()
   fi
 }
 
-install_ros()
-{
-  sudo apt-get install -y \
-       python-catkin-pkg \
-       python-rosdep \
-       python-rosinstall-generator \
-       python-wstool \
-       python-rosinstall \
-       build-essential \
-       cmake
-}
-
 main()
 {
   pushd .
-  if ! sudo --validate; then
-    error_exit "Failed to obtain elevated permissions"
-  fi
-  #get_sources
+  # install-deps
+  get_sources
   prepare_kernel
   build_kernel
   popd
